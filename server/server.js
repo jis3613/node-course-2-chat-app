33cd643362d2f97412ca46d62595c43b904a8355
jis3613 <jis3613@gmail.com>
2017-06-30 03:03:18 +0200

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -27,27 +28,30 @@ io.on('connection',(socket)=>{
   //   createdAt: 123
   // });
 
-  socket.emit('newMessage',{
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+  // socket.emit('newMessage',{
+  //   from: 'Admin',
+  //   text: 'Welcome to the chat app',
+  //   createdAt: new Date().getTime()
+  // });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage',{
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  });
+
+  // socket.broadcast.emit('newMessage',{
+  //   from: 'Admin',
+  //   text: 'New user joined',
+  //   createdAt: new Date().getTime()
+  // });
+  socket.broadcast.emit('newMessage',generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage',(message) => {
     console.log('createMessage',message);
-
+    io.emit('newMessage',generateMessage(message.from, message.text));
     //io.emit은 emit event to every single connection
-    io.emit('newMessage',{
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    // io.emit('newMessage',{
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
 
     //broadcast 나뺴고 나머지한테 다 보내는거
     // socket.broadcast.emit('newMessage',{
